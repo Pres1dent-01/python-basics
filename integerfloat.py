a625b3925967fbbd0962dba7ed10d33acf8dcdd3
Abhijit Dhara <51172366+cybertron21@users.noreply.github.com>
2019-12-24 23:38:49 +0530

    Add files via upload

---
+++ integerfloat.py
@@ -0,0 +1,20 @@
+num = 3
+print(type(num))
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+print(3//2)  #floor division
+print(3**2)
+print(3%2)
+num = 3.5
+num +=10
+print(num)
+print(abs(-5))
+print(round(3.45))
+print(round(3.45, 1)) # number of digit upto which it has to be rounded off
+
+
+num1 = '100'
+num2 = '200'
+print(int(num1) + int(num2))
